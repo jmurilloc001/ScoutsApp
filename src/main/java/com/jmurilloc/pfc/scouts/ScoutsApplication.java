87c7ff69abbcdc271883d535e6bdf33e5e33c8c5
jorge murillo <jmurilloc@minsait.com>
2025-02-07 13:13:13 +0100

    first commit

---
+++ src/main/java/com/jmurilloc/pfc/scouts/ScoutsApplication.java
@@ -0,0 +1,13 @@
+package com.jmurilloc.pfc.scouts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmurilloc/pfc/scouts/ScoutsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmurilloc.pfc.scouts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
